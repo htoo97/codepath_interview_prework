51334fef7884748b68fc6d5d72e55decd33df8d6
htoo97 <htoo97@gmail.com>
2017-07-04 00:09:02 +0630

    kth smallest efficiency solved with maxheap

--- Checkpoint3_kthsmallest.java
+++ Checkpoint3_kthsmallest.java
@@ -7,32 +7,69 @@
 public class Solution {
     // DO NOT MODIFY THE ARGUMENTS WITH "final" PREFIX. IT IS READ ONLY
     public int kthsmallest(final int[] A, int B) {
-        int temp[] = new int[B];
+        int heap[] = new int[B];
 
         for (int i=0; i<B; i++) {
-            temp[i] = A[i];    
+            heap[i] = A[i];    
+            int parent;   // integer division - floor
+            int current = i;
+            while ((parent = (current-1)/2) >= 0 && heap[parent] < heap[current]) {
+                // swap parent and child
+                int temp = heap[parent];
+                heap[parent] = heap[current];
+                heap[current] = temp;
+                current = parent;
+            }
         }
 
         for (int i=B; i<A.length; i++) {
-            int maxIndex = findMaxIndex(temp);
-            if (A[i] < temp[maxIndex]) {
-                temp[maxIndex] = A[i];
+            // compare with heap of max heap
+            if (A[i] < heap[0]) {
+                heap[0] = A[i];
+
+                int j=0;
+                int left, right, swapIndex;
+                // trickle down new element at root
+                // get larger element among the two children
+
+                do {
+                    left = 2*j + 1;
+                    right = 2*j + 2;
+
+                    // both left and right in bounds
+                    if (left < heap.length && right < heap.length) {
+                        swapIndex = (heap[left] > heap[right])? left : right;
+                    }
+                    // right out of bounds
+                    else if (left < heap.length) {
+                        swapIndex = left;
+                    }
+                    // left out of bounds
+                    else if (right < heap.length) {
+                        swapIndex = right;
+                    }
+                    // no left or right children
+                    else {
+                        break;
+                    }
+
+                    if (heap[j] < heap[swapIndex]) {
+                        // swap 
+                        int temp = heap[j];
+                        heap[j] = heap[swapIndex];
+                        heap[swapIndex] = temp;
+                        j = swapIndex;
+                    }
+                    else {
+                        break;
+                    }
+
+                } while (j < heap.length);
             }
         }
 
-        return temp[findMaxIndex(temp)];
-    }
-
-    // helper function to find maximum in temporary array
-    public int findMaxIndex(final int[] A) {
-        int maxIndex = 0;
-        for (int i=1; i<A.length; i++) {
-            if (A[i] > A[maxIndex]) {
-                maxIndex = i;
-            }
-        }
-
-        return maxIndex;
+        // return kth smallest at root of max heap of k elements
+        return heap[0];
     }
 }
 
